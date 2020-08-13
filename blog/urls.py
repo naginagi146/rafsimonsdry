0d3a1c9e0fe7bcfa38dcece8f353f0ae4aeffa2d
shimizuyuusei <naginagi146@gmail.com>
2020-08-13 12:03:21 +0900

    フォーム

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,7 @@ from django.conf.urls.static import static #追加
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) #追加
