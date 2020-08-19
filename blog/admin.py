5af31ce9e9eb69ceed0df81aaadd5adb19e0f2f7
shimizuyuusei <naginagi146@gmail.com>
2020-08-19 15:49:40 +0900

    rafsimonsdry

--- blog/admin.py
+++ blog/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 from .models import Post
 from .models import Post, Comment
 
-admin.site.register(Post)
-admin.site.register(Comment)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'text', 'published_date', 'created_date', 'updated_at')
+    ordering = ('-created_at',)
+    list_filter = ('published_date',)
+    search_fields = ('title', 'text')
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, PostAdmin)
